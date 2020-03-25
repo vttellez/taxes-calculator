56c1ab0d43d6fb5aa22b1c1dd8b5564915579d37
Vladmir Varona <vvtellez@hotmail.com>
2020-03-25 08:25:36 -0400

    Adding integration test sample

---
+++ TaxesCalculator.Tests/TaxJarCalculatorServiceIntegration.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Taxes.Services.Model;
+using TaxesCalculator.Api;
+
+namespace TaxesCalculator.Tests
+{
+    public class TestingWebAppFactory<T> : WebApplicationFactory<Startup>
+    {
+    }
+
+    [TestClass]
+    public class TaxJarCalculatorServiceIntegration
+    {
+
+        private HttpClient _client;
+
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _client = new TestingWebAppFactory<Startup>().CreateClient();
+        }
+
+        [TestMethod]
+        public async Task Get()
+        {
+            //Arrange
+            var responseJson = await _client.GetStringAsync("rates/40218");
+
+            //Act
+            var actualResponse = JsonConvert.DeserializeObject<Rate>(responseJson);
+
+            //Assert
+            Assert.IsNotNull(actualResponse);
+        }
+    }
+}
